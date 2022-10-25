97a013e18100e0125ee3c67bcbbad100809644da
Andrey Grechko <stig1467@gmail.com>
2022-10-25 03:03:54 +0300

    overview page done

--- src/views/OverviewView.vue
+++ src/views/OverviewView.vue
@@ -6,7 +6,7 @@
           <account-cloud class="ma-2 mr-16 elevation-2"/>
       </v-row>
       <v-row class="mt-8" justify="start">
-        <v-col xs12 md6 lg6 xl4 >
+        <v-col xs12 md6 lg6 xl4 class="mb-4">
           <v-card
               elevation="2"
               min-width="300"
@@ -16,12 +16,12 @@
             <v-card-title>Environments</v-card-title>
 
             <v-list
-              v-model="selectedEnvIndex"
+              :selected="[selectedEnvironment.name]"
             >
               <v-list-item
                   v-for="item in environments"
-                  :key="item.title"
-                  :value="item"
+                  :key="item.name"
+                  :value="item.name"
                   @click="selectEnvironment(item)"
                   active-color="#5777FAFF"
 
@@ -52,6 +52,112 @@
           </v-row>
         </v-col>
       </v-row>
+
+      <v-row>
+        <v-col cols="8">
+          <v-card
+              elevation="2"
+              style="border-radius: 24px;"
+              height="300"
+          >
+            <v-card-title>Information</v-card-title>
+
+<!--            <v-sheet-->
+<!--                color="#5777FA6F"-->
+<!--                height="100%"-->
+<!--                width="100%"-->
+<!--            >-->
+<!--            </v-sheet>-->
+
+
+            <v-list v-model:opened="open" density="compact" style="height: 85%; overflow: scroll">
+              <v-list-item prepend-icon="mdi-clock" title="Uptime">
+                <v-list-item-subtitle>11d 23h 06m</v-list-item-subtitle>
+              </v-list-item>
+
+              <v-list-group value="Cost">
+                <template v-slot:activator="{ props }">
+                  <v-list-item
+                      v-bind="props"
+                      prepend-icon="mdi-currency-usd"
+                      title="Cost"
+                  >
+                    <v-list-item-subtitle>$15.60</v-list-item-subtitle>
+                  </v-list-item>
+                </template>
+
+                <v-list-item
+                    v-for="(item, i) in selectedEnvironment.hosts"
+                    :key="i"
+                    :title="item.name"
+                    prepend-icon="mdi-server"
+                >
+                  <v-list-item-subtitle>{{ item.cost }}</v-list-item-subtitle>
+                </v-list-item>
+              </v-list-group>
+
+              <v-list-item
+                  title="Last Snapshot"
+                  prepend-icon="mdi-backup-restore"
+              >
+                <v-list-item-subtitle>22.10.2022 12:00:00</v-list-item-subtitle>
+              </v-list-item>
+
+              <v-list-group value="Versions">
+                <template v-slot:activator="{ props }">
+                  <v-list-item
+                      v-bind="props"
+                      title="Version"
+                      prepend-icon="mdi-xml"
+                  >
+                    <v-list-item-subtitle>1.2.0</v-list-item-subtitle>
+                  </v-list-item>
+                </template>
+
+                <v-list-item
+                    v-for="i in [5, 4, 3, 2, 1]"
+                    :key="i"
+                    :title="'Version ' + i"
+                >
+                  <v-list-item-subtitle>1.1.{{ i }}</v-list-item-subtitle>
+                </v-list-item>
+              </v-list-group>
+
+              <v-list-item
+                  title="Other"
+                  prepend-icon="mdi-information"
+                  @click="fullInfoClicked"
+              >
+                <v-list-item-subtitle>Full information about environment</v-list-item-subtitle>
+              </v-list-item>
+            </v-list>
+
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card
+              elevation="2"
+              style="border-radius: 24px"
+              height="100%"
+              class="pb-2"
+          >
+            <v-card-title>Actions</v-card-title>
+
+            <div class="btns mt-n8 centered">
+              <v-btn
+                  v-for="item in 10"
+                  :key="item"
+                  color="#5777FAFF"
+                  class="ma-2"
+              >
+                <span class="text-white">Action {{item}}</span>
+              </v-btn>
+            </div>
+
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </main>
 </template>
@@ -75,6 +181,17 @@ export default defineComponent({
     accountCloud
   },
   data: () => ({
+    open: ['Users'],
+    admins: [
+      ['Management', 'mdi-account-multiple-outline'],
+      ['Settings', 'mdi-cog-outline'],
+    ],
+    cruds: [
+      ['Create', 'mdi-plus-outline'],
+      ['Read', 'mdi-file-outline'],
+      ['Update', 'mdi-update'],
+      ['Delete', 'mdi-delete'],
+    ],
     environments: [
       {
         name: 'Production',
@@ -85,19 +202,22 @@ export default defineComponent({
             name: 'Host 1',
             description: 'Host 1 description',
             icon: 'mdi-server',
-            status: 'online'
+            status: 'online',
+            cost: '$10.00',
           },
           {
             name: 'Host 2',
             description: 'Host 2 description',
             icon: 'mdi-server',
-            status: 'online'
+            status: 'online',
+            cost: '$5.60',
           },
           {
             name: 'Host 3',
             description: 'Host 3 description',
             icon: 'mdi-server',
-            status: 'offline'
+            status: 'offline',
+            cost: '$0.00',
           },
         ],
         services: [
@@ -312,9 +432,26 @@ export default defineComponent({
       console.log(item)
       this.selectedHost = item
     },
+    fullInfoClicked() {
+      console.log('full info clicked')
+    },
   }
 });
 </script>
 
 <style scoped>
+  .btns {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    flex-direction: row;
+  }
+
+  div.centered {
+    position: relative;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+  }
 </style>
